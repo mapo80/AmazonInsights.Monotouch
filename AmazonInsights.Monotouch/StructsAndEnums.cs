041f326eae4258be0419102ffc06bb52d8f6706e
Mapo80 <mapo80@gmail.com>
2014-01-03 11:59:01 +0100

    Initial check-in of module AmazonInsights.Monotouch

---
+++ AmazonInsights.Monotouch/StructsAndEnums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AmazonInsights.Monotouch
+{
+	public enum AICompletionHandler {
+		ABTestClientErrorCode_NoProjectNamesProvided = 0,
+		ABTestClientErrorCode_ProjectNamesNil
+	}
+	public enum AIABTestClientErrorCodes {
+		NoProjectNamesProvided = 0,
+		ProjectNamesNil
+	}
+}
+
